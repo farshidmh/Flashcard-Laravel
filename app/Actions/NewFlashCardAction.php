4ef581bde3b692816ad66b27b9b3c2c7420e37c2
Farshid Mehrtash <mehrtash.fm@live.com>
2023-06-07 13:51:25 +0300

    Action to create a flashcard

---
+++ app/Actions/NewFlashCardAction.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\FlashCard;
+use App\Repositories\Interfaces\FlashcardRepository;
+
+/**
+ * This class is responsible for creating a new flashcard.
+ *
+ * @author Farshid Mehrtash
+ * @version 1.0.0
+ */
+class NewFlashCardAction
+{
+
+    private FlashcardRepository $flashcardRepository;
+
+    /**
+     * inject the flashcard repository dependency.
+     *
+     * @return void
+     */
+    public function __construct(FlashcardRepository $flashcardRepository)
+    {
+        $this->flashcardRepository = $flashcardRepository;
+    }
+
+    /**
+     * This method creates a new flashcard and returns it.
+     *
+     * @param $question
+     * @param $answer
+     * @param $caseSensitive
+     * @return FlashCard
+     *
+     * @author Farshid Mehrtash
+     * @version 1.0.0
+     *
+     */
+    public function execute($question, $answer, $caseSensitive): FlashCard
+    {
+        $caseSensitive = $caseSensitive == 'yes';
+        return $this->flashcardRepository->createFlashcard($question, $answer, $caseSensitive);
+    }
+}
